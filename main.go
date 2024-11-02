ce423ca880b450f553acbb1adb4cdc99a3ec9d12
Rouchdane03 <114878268+Rouchdane03@users.noreply.github.com>
2024-11-02 21:36:18 +0100

    main.go a evolue entretemps

--- main.go
+++ main.go
@@ -1,2 +1,12 @@
 package main
-func main(){}
+import "fmt"
+
+// Fonction pour calculer la somme de deux entiers
+func add(a int, b int) int {
+    return a + b
+}
+func main(){
+  // Appel de la fonction avec des valeurs
+    result := add(3, 5)
+    fmt.Println("La somme est :", result) // Affiche "La somme est : 8"
+}
